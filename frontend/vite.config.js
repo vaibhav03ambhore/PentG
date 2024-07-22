ac76759c9b9e8432086829dc26fa01a27ae4e30d
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-07-22 19:15:06 +0530

    deploy fixing error in this branch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // server:{
-  //   proxy:{
-  //     '/api/': 'https://pentg-backend.onrender.com/'
-  //   }
-  // },
+  server:{
+    proxy:{
+      '/api/': 'https://pentg-backend.onrender.com/'
+    }
+  },
   build: {
     outDir: 'dist'
   }
