2a69408393275c883b5fb2e2439d9e3819048d68
Vaibhav Ambhore <115728010+vaibhav03ambhore@users.noreply.github.com>
2024-07-22 17:29:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,10 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server:{
-    proxy:{
-      '/api/': 'https://pentg-backend.onrender.com/'
-    }
-  }
 })
 
