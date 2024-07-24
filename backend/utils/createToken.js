29ac2174c30b5230e0676a89f835b19415bae7c8
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-07-24 19:17:33 +0530

    set domain to 'pentg-kr1l.onrender.com'

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -9,7 +9,7 @@ const createToken = (res,userId)=>{
         httpOnly:true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite:'none',
-        domain: process.env.NODE_ENV !== 'development' ? '.onrender.com' : 'localhost',
+        domain: process.env.NODE_ENV !== 'development' ? 'pentg-kr1l.onrender.com' : 'localhost',
         maxAge:30*24*60*60*1000
     }
     //set jwt as an httpOnly cookie
